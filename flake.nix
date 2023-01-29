0a0e3cd32ee6268baaa0663cf8d7bddb6aee614c
iguoliveira <igor.or042@gmail.com>
2023-01-28 22:11:58 -0300

    add: Imports in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,12 +2,13 @@
   description = "Personal OS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     abehidek.url = "github:abehidek/nix-config";
+    misterio77.url = "github:misterio77/nix-config";
   };
 
   outputs = {
