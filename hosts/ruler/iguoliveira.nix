bdacc085bf6d88a02aa33cf1645ac7e6fc37d749
iguoliveira <igor.or042@gmail.com>
2023-01-28 23:52:38 -0300

    add: All monarch's features to ruler host

--- hosts/ruler/iguoliveira.nix
+++ hosts/ruler/iguoliveira.nix
@@ -1,6 +1,30 @@
-{ lib, config, pkgs, ... }: {
+{ lib, config, pkgs, inputs, outputs, ... }:
+let
+    colorScheme = inputs.nix-colors.colorSchemes.dracula.colors;
+in {
+    imports = [
+        inputs.misterio77.homeManagerModules.fonts
+        inputs.abehidek.userModules.desktop
+        inputs.nix-colors.homeManagerModule
+    ];
+
+    modules.user.desktop = {
+        term.kitty = {
+            enable = true;
+            font = {
+                enable = true;
+                family = "FiraCode Nerd Font";
+                package = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
+            };
+            colors = {
+                enable = true;
+                base16 = colorScheme;
+            };
+        };
+    };
+
     home = {
-        stateVersion = "23.05"; # do not change this
+        stateVersion = "23.05"; # DO NOT TOUCH
         username = "iguoliveira";
         homeDirectory = "/home/iguoliveira";
         packages = with pkgs; [
@@ -9,10 +33,23 @@
             brave
             nodejs
             git
-            starship
             vscode
             nodePackages_latest.pnpm
             neofetch
         ];
     };
+
+    fontProfiles = {
+        enable = true;
+        monospace = {
+            family = "FiraCode Nerd Font";
+            package = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
+        };
+        regular = {
+            family = "Fira Sans";
+            package = pkgs.fira;
+        };
+    };
+
+    programs.starship.enable = true;
 }
