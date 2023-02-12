7e57effb702db7e80e1065f2c310649db1d1dbc2
iguoliveira <igor.or042@gmail.com>
2023-02-12 10:05:27 -0300

    add(titan): Config new env

---
+++ hosts/titan/default.nix
@@ -0,0 +1,82 @@
+{ config, pkgs, inputs, outputs, lib, ... }:
+
+{
+  imports =
+  [
+    ../global.nix
+    ./hardware.nix
+    inputs.home-manager.nixosModules.home-manager
+    inputs.abehidek.nixosModules.shell
+  ];
+
+  modules.system = {
+    shell = {
+      zsh = {
+        enable = true;
+        rice = true;
+        users = [ "iguoliveira" ];
+      };
+      direnv = {
+        enable = true;
+        users = [ "iguoliveira" ];
+      };
+    };
+
+    services = {
+      docker = {
+        users = [ "iguoliveira" ];
+      };
+    };
+  };
+
+  users.users.iguoliveira = {
+    isNormalUser = true;
+    description = "Igor Oliveira";
+    extraGroups = [ "networkmanager" "wheel" "adbusers" ];
+  };
+
+  programs = {
+    adb.enable = true;
+  };
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users = {
+      iguoliveira = import ./iguoliveira.nix;
+    };
+    extraSpecialArgs = { inherit inputs outputs; };
+  };
+
+  boot.loader = {
+    efi = {
+     canTouchEfiVariables = true;
+     efiSysMountPoint = "/boot/efi";
+    };
+    grub = {
+     devices = [ "nodev" ];
+     efiSupport = true;
+     enable = true;
+     useOSProber = true;
+     version = 2;
+    };
+  };
+
+  networking = {
+    networkmanager.enable = true; # Enable networking
+    hostName = "titan";
+  };
+
+  services.xserver = {
+      layout = "us";
+      xkbVariant = "altgr-intl";
+  };
+
+  console.keyMap = "br-abnt2";
+
+  nixpkgs.config = {
+    allowNonFree = true;
+  };
+
+  system.stateVersion = "22.11"; # Careful
+}
